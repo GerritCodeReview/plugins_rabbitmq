d89a4903ea6b039baa3a1b15d55358f474226f3e
David Pursehouse <david.pursehouse@sonymobile.com>
2015-04-20 09:08:03 +0900

    Add timestamp on messages
    If the timestamp is not set, clients such as Logstash will tag the
    message with the current timestamp when they are consumed.
    
    Change-Id: Ie08a8b2528fdd3f99e33dfd6f52c80ced74d74d3

--- src/main/java/com/googlesource/gerrit/plugins/rabbitmq/config/AMQProperties.java
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/config/AMQProperties.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -79,6 +80,7 @@ public class AMQProperties {
         .deliveryMode(message.deliveryMode)
         .priority(message.priority)
         .headers(headers)
+        .timestamp(new Date())
         .build();
     }
     return amqpProperties;
