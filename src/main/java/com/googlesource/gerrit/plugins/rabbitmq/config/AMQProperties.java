c59742009c72314e9c4ee0f6df3fd706038b07ad
Sven Selberg <sven.selberg@sonymobile.com>
2015-05-19 12:58:25 +0200

    Change timestamp for each publication
    In AMQProperties.getBasicProperties() timestamp was only set the first
    time it was called.
    
    Since all 'set' methods are deprecated, the amqpProperties is built
    again from a copy of the old amqpProperties.
    
    Change-Id: I09489171c40b6c1e04ed54d586e4cdfc81eed38f

--- src/main/java/com/googlesource/gerrit/plugins/rabbitmq/config/AMQProperties.java
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/config/AMQProperties.java
@@ -83,6 +83,17 @@ public class AMQProperties {
         .headers(headers)
         .timestamp(new Date(TimeUtil.nowMs()))
         .build();
+    } else {
+      // Set a new timestamp.
+      amqpProperties = new AMQP.BasicProperties.Builder()
+        .appId(amqpProperties.getAppId())
+        .contentEncoding(amqpProperties.getContentEncoding())
+        .contentType(amqpProperties.getContentType())
+        .deliveryMode(amqpProperties.getDeliveryMode())
+        .priority(amqpProperties.getPriority())
+        .headers(amqpProperties.getHeaders())
+        .timestamp(new Date(TimeUtil.nowMs()))
+        .build();
     }
     return amqpProperties;
   }
