ec02f43b79865342f1eea0dbd5b0cea12436850b
Sven Selberg <svense@axis.com>
2017-11-27 11:56:39 +0100

    Name the publisher thread
    Change-Id: Idad5e0d27fe6d04d90094583949f332bce61828e

--- src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/MessagePublisher.java
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/MessagePublisher.java
@@ -122,6 +122,7 @@ public class MessagePublisher implements Publisher, LifecycleListener {
   @Override
   public void start() {
     publisherThread = new Thread(publisher);
+    publisherThread.setName("rabbitmq-publisher");
     publisherThread.start();
     if (!isConnected()) {
       connect();
